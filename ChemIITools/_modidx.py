55aa5c33ab6cc792a3cbc4f30cdf182c3a56885c
KGrewal1 <45569241+KGrewal1@users.noreply.github.com>
2022-10-14 13:34:16 +0100

    Add Huckel Solver

--- ChemIITools/_modidx.py
+++ ChemIITools/_modidx.py
@@ -5,4 +5,4 @@ d = { 'settings': { 'branch': 'main',
                 'doc_host': 'https://KGrewal1.github.io',
                 'git_url': 'https://github.com/KGrewal1/ChemIITools',
                 'lib_path': 'ChemIITools'},
-  'syms': {'ChemIITools.core': {'ChemIITools.core.foo': ('core.html#foo', 'ChemIITools/core.py')}}}
+  'syms': {'ChemIITools.core': {'ChemIITools.core.foo': ('huckelsolver.html#foo', 'ChemIITools/core.py')}}}
